90e36b2e0649a103917cbcf5023ea92e4434b638
fabiohsmarques16-stack <fabiohsmarques16@gmail.com>
2026-05-07 22:33:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,25 @@
 // 3) Ative Firestore Database
 // 4) Cole abaixo as credenciais do app web
 
-window.FIREBASE_CONFIG = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPFCGphpAXmCDMvYfrnmT1aMyzV1UmcPk",
+    authDomain: "assistente-pessoal-ia-6edc7.firebaseapp.com",
+    projectId: "assistente-pessoal-ia-6edc7",
+    storageBucket: "assistente-pessoal-ia-6edc7.firebasestorage.app",
+    messagingSenderId: "64235509575",
+    appId: "1:64235509575:web:0abdcb667f38acbead1e62"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
 
 // IA: nunca coloque sua chave da OpenAI direto no app.
 // Use um backend seguro. Exemplo: Cloudflare Worker, Firebase Function ou servidor Node.
